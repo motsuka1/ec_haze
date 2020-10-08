e6cf5326c4ce992118211ce3bf47082b92496222
Masaki Otsuka <motsuka@soka.edu>
2020-10-08 19:04:20 +0900

    fix indent in store.html

--- ec_haze/settings.py
+++ ec_haze/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'ec_haze.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "store/templates")],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
